6d908a14ad69a9ade8dec90c6c0592e963b4dae0
THARANIKA-R <sudhatharanika@gmail.com>
2026-03-08 21:39:19 +0530

    Updated Requirements.txt

--- Frontend/frontend/src/api.js
+++ Frontend/frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchProducts = async (limit = 100) => {
   try {
-    const response = await axios.get(`http://127.0.0.1:8000/products`);
+    const response = await axios.get(`https://ackend-tharanika-r-git7566-o5x6v5ff.leapcell.dev/products`);
     return response.data;
   } catch (error) {
     console.error("Error fetching products:", error);
